25f8bac2ed4d37f78abd178a2fd87fac0313c0c8
Filip5011 <filipmc5011@gmail.com>
2026-08-04 11:57:49 +0100

    Initial command and setup complete

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is ready and running.")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def wiki(ctx):
+    await ctx.send("Here is the Official Loomian Legacy Wiki:\nhttps://loomian-legacy.fandom.com/wiki/Loomian_Legacy_WikiS")
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)

47f6b9c5da8019aa51f414b059bd2ee47f0733b5
Filip5011 <filipmc5011@gmail.com>
2026-08-04 13:00:25 +0100

    Basic spawning function + additional future content

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 import logging
 from dotenv import load_dotenv
+import json
+import random
+from loomian_dict import loomian_dict
+from loomians import loomians
 import os
 
 load_dotenv()
@@ -12,7 +16,13 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+MIN_MESSAGES = 3
+MAX_MESSAGES = 10
+
+message_count = 0
+next_spawn_interval = random.randint(MIN_MESSAGES, MAX_MESSAGES)
+
+bot = commands.Bot(command_prefix=commands.when_mentioned, intents=intents)
 
 @bot.event
 async def on_ready():
@@ -23,10 +33,22 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
+    message_count += 1
+
+    if message_count >= next_spawn_interval:
+        await message.channel.send(random.choice(loomians))
+        message_count = 0
+        next_spawn_interval = random.randint(MIN_MESSAGES, MAX_MESSAGES)
+
     await bot.process_commands(message)
 
 @bot.command()
 async def wiki(ctx):
     await ctx.send("Here is the Official Loomian Legacy Wiki:\nhttps://loomian-legacy.fandom.com/wiki/Loomian_Legacy_Wiki")
+
+# @bot.command()
+# @commands.has_permissions(administrator=True)
+# async def setup(ctx):
+
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
